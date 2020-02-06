3bee456efac22f41dc2f530c0fafc7cbe9099eb6
Ryan Craig <rcraig@melaleuca.com>
2020-02-05 22:51:41 -0700

    Update ad.tf

--- ad.tf
+++ ad.tf
@@ -18,5 +18,5 @@ resource "aws_vpc_dhcp_options" "my_microsoftad_dhcp" {
 
 resource "aws_vpc_dhcp_options_association" "my_microsoftad_dns_resolver" {
   vpc_id          =  var.vpc_id
-  dhcp_options_id = ${aws_vpc_dhcp_options.my_microsoftad_dhcp.id}"
+  dhcp_options_id = "${aws_vpc_dhcp_options.my_microsoftad_dhcp.id}"
 }
