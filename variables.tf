59d686bf00954c00cdac9e7461daf73d36248e0f
Ryan Craig <rcraig@melaleuca.com>
2020-02-05 22:41:33 -0700

    Update Variables.tf
    Update variables.tf for terraform v 0.12

--- variables.tf
+++ variables.tf
@@ -1,36 +1,43 @@
 variable "domain_name" {
   description = "the domain name of the active directory service."
+  type = string
 }
 
 variable "short_name" {
   description = "the short name of the active directory service."
+  type = string
 }
 
 
 variable "computer_ou" {
-  default = ""
   description = "the org unit of the active directory service."
+  type = string
 }
 
 variable "edition" {
   default = "Standard"
   description = "the edition of the MicrosoftAD active directory service. Standard or Enterprise"
+  type = string
 }
 
 variable "admin_password" {
   description = "the admin password of the active directory service."
+  type = string
 }
 
 variable "vpc_id" {
   description = "the vpc id of the active directory service."
+  type = string
 }
 
 variable "subnet_ids" {
-  type = "list"
+  type = list(string)
   description = "the subnet ids of the active directory service."
+  default = [""]
 }
 
 variable "iam_prefix" {
   default = "ec2-ssm"
   description = "prefix for naming iam resources."
+  type = string
 }
